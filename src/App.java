a5d157fd217df4a51359f25d50ccae17c70889e0
desmonduz <sarrtv@gmail.com>
2021-01-11 09:54:12 +0500

    added new examples

---
+++ src/App.java
@@ -0,0 +1,10 @@
+import browser.ReadServerFile;
+
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        ReadServerFile app = new ReadServerFile();
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
